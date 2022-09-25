c928e59366231ff610bf7a0a132e61b4e6d9adf6
kampersanda <shnsk.knd@gmail.com>
2022-09-25 23:15:17 +0900

    chunking

--- src/bitset.rs
+++ src/bitset.rs
@@ -1,25 +1,55 @@
 use std::ops::Range;
 
 pub struct Bitset {
-    bits: Vec<bool>,
+    bits: Vec<u64>,
 }
 
 impl Bitset {
     pub fn new(len: usize) -> Self {
         Self {
-            bits: vec![false; len],
+            bits: vec![0; Self::words_for(len)],
         }
     }
 
     #[inline(always)]
     pub fn get(&self, i: usize) -> bool {
-        self.bits[i]
+        let (block, shift) = (i / 64, i % 64);
+        (self.bits[block] >> shift) & 1 == 1
     }
 
     #[inline(always)]
     pub fn set_range(&mut self, r: Range<usize>) {
-        for i in r {
-            self.bits[i] = true
+        if r.end == 0 {
+            return;
         }
+
+        let start_shift = r.start % 64;
+        let end_shift = r.end % 64;
+        let start_block = r.start / 64;
+        let last_block = if end_shift == 0 {
+            r.end / 64 - 1
+        } else {
+            r.end / 64
+        };
+
+        for block in start_block..=last_block {
+            let mut mask = u64::MAX;
+            if block == start_block {
+                // Clear the first start_shift bits.
+                mask >>= start_shift;
+                mask <<= start_shift;
+            }
+            if block == last_block && end_shift != 0 {
+                // Clear the bits other than the first end_shift bits.
+                mask <<= 64 - end_shift;
+                mask >>= 64 - end_shift;
+            }
+            self.bits[block] |= mask;
+        }
+    }
+
+    #[inline(always)]
+    const fn words_for(n: usize) -> usize {
+        (n + 63) / 64
     }
 }
