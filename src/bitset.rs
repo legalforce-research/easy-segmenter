5d0fa0ec24e3daffc3c2389edb4f048f05599219
kampersanda <shnsk.knd@gmail.com>
2022-09-25 23:24:22 +0900

    add test

--- src/bitset.rs
+++ src/bitset.rs
@@ -53,3 +53,62 @@ impl Bitset {
         (n + 63) / 64
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_set_range_1() {
+        let mut bitset = Bitset::new(200);
+        bitset.set_range(0..200);
+        for i in 0..200 {
+            assert!(bitset.get(i));
+        }
+    }
+
+    #[test]
+    fn test_set_range_2() {
+        let mut bitset = Bitset::new(200);
+        bitset.set_range(10..190);
+        for i in 0..10 {
+            assert!(!bitset.get(i));
+        }
+        for i in 10..190 {
+            assert!(bitset.get(i));
+        }
+        for i in 190..200 {
+            assert!(!bitset.get(i));
+        }
+    }
+
+    #[test]
+    fn test_set_range_3() {
+        let mut bitset = Bitset::new(200);
+        bitset.set_range(64..128);
+        for i in 0..64 {
+            assert!(!bitset.get(i));
+        }
+        for i in 64..128 {
+            assert!(bitset.get(i));
+        }
+        for i in 128..200 {
+            assert!(!bitset.get(i));
+        }
+    }
+
+    #[test]
+    fn test_set_range_4() {
+        let mut bitset = Bitset::new(200);
+        bitset.set_range(65..127);
+        for i in 0..65 {
+            assert!(!bitset.get(i));
+        }
+        for i in 65..127 {
+            assert!(bitset.get(i));
+        }
+        for i in 127..200 {
+            assert!(!bitset.get(i));
+        }
+    }
+}
