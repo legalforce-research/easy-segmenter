78d89b6be9b8ca8ca9d2947dd98803ba420ef60b
kampersanda <shnsk.knd@gmail.com>
2022-09-25 13:55:08 +0900

    add dump

---
+++ benchmark/easy-segmenter-bench/src/dump.rs
@@ -0,0 +1,18 @@
+use std::io::Read;
+
+use easy_segmenter::SegmenterBuilder;
+
+fn main() {
+    let mut text = String::new();
+    std::io::stdin().read_to_string(&mut text).unwrap();
+
+    let seg = SegmenterBuilder::new()
+        .in_periods(["。", "?", "!"])
+        .ex_periods(["\n"])
+        .parentheses([('(', ')'), ('「', '」')])
+        .build()
+        .unwrap();
+    for (i, j) in seg.segment(&text) {
+        println!("{}", &text[i..j]);
+    }
+}
